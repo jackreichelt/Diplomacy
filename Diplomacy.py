d0bce10305e8f64c19a29bb2c48645b257dd7a99
jackreichelt <jackreichelt@gmail.com>
2015-12-08 20:49:42 +1000

    More tests
    More tests created for edge cases.

--- Diplomacy.py
+++ Diplomacy.py
@@ -558,6 +558,217 @@ class LandLockedThreeFactionTests(unittest.TestCase):
 		self.assertEqual(self.testLocationC.owner, 3)
 		self.assertEqual(self.testLocationD.owner, 7)
 
+class LandLockedTriangleLoop(unittest.TestCase):
+	def setUp(self):
+		self.testGame = Game(True)
+		self.testGame.regions = []
+		self.testGame.units = []
+		self.testGame.regionDict = {}
+
+		self.testLocationA = Region('aaa', 'aaa', 0, 1)
+		self.testLocationB = Region('bbb', 'bbb', 0, 2)
+		self.testLocationC = Region('ccc', 'ccc', 0, 3)
+		"""
+		Test Region Layout
+		-----
+		|A|B|
+		-----
+		| C |
+		-----
+		All regions are land.
+		Diagonal regions ARE NOT adjacent.
+		Regions A, B, and C are all owned by different people.
+		Region A has a unit in it.
+		Region B has a unit in it.
+		Region C has a unit in it.
+		"""
+		self.testGame.addRegion(self.testLocationA)
+		self.testGame.addRegion(self.testLocationB)
+		self.testGame.addRegion(self.testLocationC)
+
+		self.testGame.connectTwoRegions('aaa', 'bbb')
+		self.testGame.connectTwoRegions('aaa', 'ccc')
+		#self.testGame.connectTwoRegions('bbb', 'ddd')
+		#self.testGame.connectTwoRegions('ccc', 'ddd')
+		#self.testGame.connectTwoRegions('aaa', 'ddd')
+		self.testGame.connectTwoRegions('bbb', 'ccc')
+
+		self.testUnitA = Unit(0, self.testLocationA, 1)
+		self.testLocationA.unit = self.testUnitA
+
+		self.testUnitB = Unit(0, self.testLocationB, 2)
+		self.testLocationB.unit = self.testUnitB
+
+		self.testUnitC = Unit(0, self.testLocationC, 3)
+		self.testLocationC.unit = self.testUnitC
+
+		self.testGame.units.append(self.testUnitA)
+		self.testGame.units.append(self.testUnitB)
+		self.testGame.units.append(self.testUnitC)
+
+	def test_ThreeLoop(self):
+		#print('<<HERE>>')
+		self.testGame.addOrder('A aaa-bbb')
+		self.testGame.addOrder('A bbb-ccc')
+		self.testGame.addOrder('A ccc-aaa')
+		self.testGame.resolveOrders()
+		self.testGame.endTurn()
+		#print('<<AND HERE>>')
+
+		self.assertEqual(self.testUnitA.location, self.testLocationB)
+		self.assertEqual(self.testUnitB.location, self.testLocationC)
+		self.assertEqual(self.testUnitC.location, self.testLocationA)
+
+		self.assertEqual(self.testLocationA.unit, self.testUnitC)
+		self.assertEqual(self.testLocationB.unit, self.testUnitA)
+		self.assertEqual(self.testLocationC.unit, self.testUnitB)
+		
+		self.assertEqual(self.testLocationA.owner, 3)
+		self.assertEqual(self.testLocationB.owner, 1)
+		self.assertEqual(self.testLocationC.owner, 2)
+
+class LandLockedSquareLoop(unittest.TestCase):
+	def setUp(self):
+		self.testGame = Game(True)
+		self.testGame.regions = []
+		self.testGame.units = []
+		self.testGame.regionDict = {}
+
+		self.testLocationA = Region('aaa', 'aaa', 0, 1)
+		self.testLocationB = Region('bbb', 'bbb', 0, 2)
+		self.testLocationC = Region('ccc', 'ccc', 0, 3)
+		self.testLocationD = Region('ddd', 'ddd', 0, 4)
+		"""
+		Test Region Layout
+		-----
+		|A|B|
+		-----
+		|D|C|
+		-----
+		All regions are land.
+		Diagonal regions ARE NOT adjacent.
+		Regions A, B, C, and D are all owned by different people.
+		Region A has a unit in it.
+		Region B has a unit in it.
+		Region C has a unit in it.
+		Region D has a unit in it.
+		"""
+		self.testGame.addRegion(self.testLocationA)
+		self.testGame.addRegion(self.testLocationB)
+		self.testGame.addRegion(self.testLocationC)
+		self.testGame.addRegion(self.testLocationD)
+
+		self.testGame.connectTwoRegions('aaa', 'bbb')
+		self.testGame.connectTwoRegions('bbb', 'ccc')
+		self.testGame.connectTwoRegions('ccc', 'ddd')
+		self.testGame.connectTwoRegions('ddd', 'aaa')
+
+		self.testUnitA = Unit(0, self.testLocationA, 1)
+		self.testLocationA.unit = self.testUnitA
+
+		self.testUnitB = Unit(0, self.testLocationB, 2)
+		self.testLocationB.unit = self.testUnitB
+
+		self.testUnitC = Unit(0, self.testLocationC, 3)
+		self.testLocationD.unit = self.testUnitC
+
+		self.testUnitD = Unit(0, self.testLocationD, 4)
+		self.testLocationD.unit = self.testUnitD
+
+		self.testGame.units.append(self.testUnitA)
+		self.testGame.units.append(self.testUnitB)
+		self.testGame.units.append(self.testUnitC)
+		self.testGame.units.append(self.testUnitD)
+
+	def test_fourloop(self):
+		#print('<<HERE>>')
+		self.testGame.addOrder('A aaa-bbb')
+		self.testGame.addOrder('A bbb-ccc')
+		self.testGame.addOrder('A ccc-ddd')
+		self.testGame.addOrder('A ddd-aaa')
+		self.testGame.resolveOrders()
+		self.testGame.endTurn()
+		#print('<<AND HERE>>')
+
+		self.assertEqual(self.testUnitA.location, self.testLocationB)
+		self.assertEqual(self.testUnitB.location, self.testLocationC)
+		self.assertEqual(self.testUnitC.location, self.testLocationD)
+		self.assertEqual(self.testUnitD.location, self.testLocationA)
+
+		self.assertEqual(self.testLocationA.unit, self.testUnitD)
+		self.assertEqual(self.testLocationB.unit, self.testUnitA)
+		self.assertEqual(self.testLocationC.unit, self.testUnitB)
+		self.assertEqual(self.testLocationD.unit, self.testUnitC)
+		
+		self.assertEqual(self.testLocationA.owner, 4)
+		self.assertEqual(self.testLocationB.owner, 1)
+		self.assertEqual(self.testLocationC.owner, 2)
+		self.assertEqual(self.testLocationD.owner, 3)
+
+class LandDiagonalUnits(unittest.TestCase):
+	def setUp(self):
+		self.testGame = Game(True)
+		self.testGame.regions = []
+		self.testGame.units = []
+		self.testGame.regionDict = {}
+
+		self.testLocationA = Region('aaa', 'aaa', 0, 1)
+		self.testLocationB = Region('bbb', 'bbb', 0, 2)
+		self.testLocationC = Region('ccc', 'ccc', 0, 3)
+		self.testLocationD = Region('ddd', 'ddd', 0, 4)
+		"""
+		Test Region Layout
+		-----
+		|A|B|
+		-----
+		|D|C|
+		-----
+		All regions are land.
+		Diagonal regions ARE NOT adjacent.
+		Regions A, B, C, and D are all owned by different people.
+		Region A has a unit in it.
+		Region C has a unit in it.
+		"""
+		self.testGame.addRegion(self.testLocationA)
+		self.testGame.addRegion(self.testLocationB)
+		self.testGame.addRegion(self.testLocationC)
+		self.testGame.addRegion(self.testLocationD)
+
+		self.testGame.connectTwoRegions('aaa', 'bbb')
+		self.testGame.connectTwoRegions('bbb', 'ccc')
+		self.testGame.connectTwoRegions('ccc', 'ddd')
+		self.testGame.connectTwoRegions('ddd', 'aaa')
+
+		self.testUnitA = Unit(0, self.testLocationA, 1)
+		self.testLocationA.unit = self.testUnitA
+
+		self.testUnitC = Unit(0, self.testLocationC, 3)
+		self.testLocationD.unit = self.testUnitC
+
+		self.testGame.units.append(self.testUnitA)
+		self.testGame.units.append(self.testUnitC)
+
+	def test_bounce(self):
+		#print('<<HERE>>')
+		self.testGame.addOrder('A aaa-bbb')
+		self.testGame.addOrder('A ccc-bbb')
+		self.testGame.resolveOrders()
+		self.testGame.endTurn()
+		#print('<<AND HERE>>')
+
+		self.assertEqual(self.testUnitA.location, self.testLocationA)
+		self.assertEqual(self.testUnitC.location, self.testLocationC)
+
+		self.assertEqual(self.testLocationA.unit, self.testUnitA)
+		self.assertEqual(self.testLocationB.unit, None)
+		self.assertEqual(self.testLocationC.unit, self.testUnitC)
+		self.assertEqual(self.testLocationD.unit, None)
+		
+		self.assertEqual(self.testLocationA.owner, 1)
+		self.assertEqual(self.testLocationB.owner, 2)
+		self.assertEqual(self.testLocationC.owner, 3)
+		self.assertEqual(self.testLocationD.owner, 4)
+
 
 if __name__ == '__main__':
 		unittest.main()
